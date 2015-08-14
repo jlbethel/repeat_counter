dd9e93384884ca4c6bf8cb507e2dab11285f6768
Jason <jlbethel@gmail.com>
2015-08-14 11:27:36 -0700

    add comments to app.php

--- app/app.php
+++ app/app.php
@@ -1,6 +1,8 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/RepeatCounter.php";
+
+    // Silex and Twig framworks for organization
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
