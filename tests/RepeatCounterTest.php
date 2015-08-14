303481c21c1df871ae9edd24cd98730732677fe5
Jason <jlbethel@gmail.com>
2015-08-14 10:01:48 -0700

    add second test to RepeatCounterTest.php

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -16,5 +16,20 @@
             //Assert
             $this->assertEquals("1", $result);
         }
+
+        function test_countRepeats_twoWord()
+        {
+            //Arrange
+            $test_RepeatCounter = new RepeatCounter;
+            $user_word = "a";
+            $user_phrase = "a dog";
+
+            //Act
+            $result = $test_RepeatCounter->countRepeats($user_word, $user_phrase);
+
+            //Assert
+            $this->assertEquals("1", $result);
+        }
+        }
     }
 ?>
