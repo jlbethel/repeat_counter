3b959ff98c0084e149f761867d23e0ef15aa9186
Jason <jlbethel@gmail.com>
2015-08-14 10:12:11 -0700

    add comments to RepeatCounter.php

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -6,8 +6,8 @@
             $result = 0;
             $phrase_array = explode(" ", $user_phrase);
 
+            //Begin foreach loop to test the user's word against the user's phrase
             foreach ($phrase_array as $word) {
-
                 if ($user_word === $word) {
                     $result += 1;
                 }
